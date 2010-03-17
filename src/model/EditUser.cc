f3fcdf2f73cf78ec50596c2f7b590e806b06c7d7
Rachel Grim <rachelgrimard@gmail.com>
2010-03-16 21:08:34 -0400

    edit user now has a working unit combobox

--- src/model/EditUser.cc
+++ src/model/EditUser.cc
@@ -15,6 +15,13 @@ EditUser::EditUser(QWidget *parent,QString usName)
 	connect( getUser, SIGNAL(clicked() ), this, SLOT( getToEdit() ) ); 
 	username = usName;
 	
+	QString queer = "SELECT unit_number FROM units";
+	QSqlQuery query2(queer);
+	while(query2.next()){
+		 queer=query2.value(0).toString();
+		 unitNum->addItem(queer);
+	}
+	
 	if(username != "coord"){
 		//saveCancel->setEnabled(false);
 		getUser->setEnabled(false);
@@ -25,7 +32,6 @@ EditUser::EditUser(QWidget *parent,QString usName)
 	 	userName->setEnabled(false);
 		userEdit->setEnabled(false);
 	 	telephone->setEnabled(false);
-	 	//unit->setText(answer[9]);
 		owing->setEnabled(false);
 		pastAddress->setEnabled(false);
 		over21->setEnabled(false);
@@ -61,11 +67,17 @@ void EditUser::getSave()
 	QString pastAdd = pastAddress->text();
 	QString pass = password->text();
 	QString owe = owing->text();
-	//QString unitx = unit->text();
-	QDate movDate = moveInDate->date();
+	QString movDate = moveInDate->date().toString();
 	bool hidden = privateTele->isChecked();
 	bool ofAge = over21->isChecked();
 	bool mvOut = movedOut->isChecked(); 
+	
+	QString unitx = unitNum->currentText();
+	QString t = "SELECT id FROM units WHERE unit_number = '" + unitx + "'";
+	QSqlQuery q(t);
+	q.next();
+	QString unit = q.value(0).toString();
+	qDebug() << " unit = " << unit;
 	
 	//cheap error detection for time being
 	if(fName.length() == 0 || lName.length() == 0 || uName.length() == 0 \
@@ -96,26 +108,24 @@ void EditUser::getSave()
 	else
 		live = '1';
 	QString text = "UPDATE users \
-	SET user_name = '" + uName +"', last_name = '" + lName + "', first_name = '" + fName + "', unit_id = '111', phone_number = '" + tele +"', phone_number_is_public = '" + phonePublic + "', is_21 = '" + age + "', is_resident = '" + live + "', \
-	 in_arrears = '" + owe + "', old_address = '" + pastAdd + "', password = '" + pass + "'  WHERE user_name = '" + userEdit->text() +"';";
+	SET user_name = '" + uName +"', last_name = '" + lName + "', first_name = '" + fName + "', unit_id = '" + unit + "', phone_number = '" + tele +"', phone_number_is_public = '" + phonePublic + "', is_21 = '" + age + "', is_resident = '" + live + "', \
+	 in_arrears = '" + owe + "', old_address = '" + pastAdd + "', password = '" + pass + "' WHERE user_name = '" + userEdit->text() + "';";
 	//test data
 	QSqlQuery query;
-	
 	qDebug() << "true?" << query.exec(text);
 	qDebug() << "first Name = " << fName;
 	qDebug() << "last Name = " << lName;
 	qDebug() << "username = " << uName;
+	qDebug() << "unit = " << unit; 
 	
 	qDebug() << "tele# = " << tele;
 	qDebug() << "pastAddress = " << pastAdd;
-//	qDebug() << "unit = " << unitx;
 	qDebug() << "date = " << movDate;	
 	qDebug() << "private? = " << hidden;
 	qDebug() << "ofAge? = " << ofAge;
 	qDebug() << "mvout? = " << mvOut;
 	this->close();
 	}
-  
 }
 
 void EditUser::deleteUse(){
@@ -153,11 +163,19 @@ void EditUser::fillMemberInfo(QString userName) {
          	answer[x] = query.value(x).toString();
         }
     }
+	qDebug() << " amswer[9] = " <<  answer[9];
+	QString text2 = "SELECT unit_number FROM units WHERE id = '" + answer[9] + "'";
+	QSqlQuery q(text2);
+	q.next();
+	QString unitNumber = q.value(0).toString();
+	qDebug() << unitNumber;
+	int index = unitNum->findText(unitNumber);
+	qDebug() << index;
+	unitNum->setCurrentIndex(index);
 	firstName->setText(answer[5]);
  	lastName->setText(answer[4]);
  	userEdit->setText(answer[2]);
  	telephone->setText(answer[10]);
- 	//unit->setText(answer[9]);
 	password->setText(answer[3]);
 	owing->setText(answer[12]);
 	pastAddress->setText(answer[13]);
@@ -180,4 +198,5 @@ void EditUser::fillMemberInfo(QString userName) {
 	else {
 		over21->setChecked(false);
 	}
+	
 }
