505d52ad5c37289ab5ea5412423fb1a87bcaef9a
jtieu <jonathan.tieu@gmail.com>
2010-03-18 13:23:59 -0400

    sets labels and promotes and stuff

--- src/model/CommitteeInformation.cc
+++ src/model/CommitteeInformation.cc
@@ -11,6 +11,7 @@ QSqlTableModel *Tmodel;
 QSqlTableModel *CLmodel;
 QString committeeName;
 QString userName;
+QString userID;
 
 CommitteeInformation::CommitteeInformation(QString committee, QString currentUser){
 	committeeName = committee;
@@ -54,7 +55,7 @@ CommitteeInformation::CommitteeInformation(QString committee, QString currentUse
 	CLmodel = new QSqlTableModel(this);
 	CLmodel->setTable("users");
 
-	CLmodel->setFilter("committee_id = " + CommitteeNum);
+	CLmodel->setFilter("committee_id = " + CommitteeNum + " and is_resident = 1");
 	//CLmodel->setFilter("is_resident = 1");
 	
 	CLmodel->setHeaderData(1, Qt::Horizontal, tr("User name"));
@@ -64,7 +65,60 @@ CommitteeInformation::CommitteeInformation(QString committee, QString currentUse
 	CLmodel->select();
 	
 	constituentView->setModel(CLmodel);
+
+	// display chair and secretary names
+	QString getChairInt  = "SELECT chair_id FROM committees WHERE id = " + CommitteeNum;
 	
+	
+	QSqlQuery query1(getChairInt);
+	
+	while ( query1.next() ) {
+		getChairInt = query1.value(0).toString();
+	}
+	if(getChairInt != NULL){
+
+		QString getChairName = "SELECT first_name, last_name FROM users WHERE id = " + getChairInt;
+	
+		QSqlQuery query2(getChairName);
+	
+		while ( query2.next() ) {
+			getChairName = query2.value(0).toString();
+			getChairName += " " + query2.value(1).toString();
+		}
+		cNameLabel->setText(getChairName);
+	} else {
+		cNameLabel->setText("None");
+	}
+
+	QString getSecretaryInt  = "SELECT secretary_id FROM committees WHERE id = " + CommitteeNum;
+	
+	QSqlQuery query3(getSecretaryInt);
+	
+	while ( query3.next() ) {
+		getSecretaryInt = query3.value(0).toString();
+	}
+	if(getSecretaryInt != NULL){
+
+		QString getSecretaryName = "SELECT first_name, last_name FROM users WHERE id = " + getSecretaryInt;
+	
+		QSqlQuery query4(getSecretaryName);
+	
+		while ( query4.next() ) {
+			getSecretaryName = query4.value(0).toString();
+			getSecretaryName += " " + query4.value(1).toString();
+		}
+		sNameLabel->setText(getSecretaryName);
+	} else {
+		sNameLabel->setText("None");
+	}
+
+	// store user's id
+	QString text = "SELECT id FROM users WHERE user_name = " + userName;
+	QSqlQuery query5(text);
+	while(query5.next()){
+		userID = query5.value(0).toString();
+	}
+
 	// hide all columns except username, last name, first name
 	constituentView->setColumnHidden(0, true);
 	constituentView->setColumnHidden(1, true);
@@ -94,14 +148,36 @@ CommitteeInformation::CommitteeInformation(QString committee, QString currentUse
 	committeeNameLabel->setText(committeeName);
 }
 void CommitteeInformation::addTask(){
+	
+	QString text = "SELECT chair_id FROM committees WHERE name = " + committeeName;
+	QSqlQuery query(text);
+	QString chairID;
+	while(query.next()){
+		chairID = query.value(0).toString();
+	}
+
+	if(chairID == userID || userName == "coord"){
 	AddTask *at = new AddTask(committeeName,this);
 	at->show();
+	}
+	else {
+		QMessageBox::warning(this, qApp->tr("AAAAAAAAAAAA!"),
+			qApp->tr("YOU CAN'T DO THAT!!!!.\n"),
+			QMessageBox::Ok);
+	}
 }
 void CommitteeInformation::refreshTask(){
 	Tmodel->select();
 }
 void CommitteeInformation::deleteTask(){
+	QString text = "SELECT chair_id FROM committees WHERE name = " + committeeName;
+	QSqlQuery query(text);
+	QString chairID;
+	while(query.next()){
+		chairID = query.value(0).toString();
+	}
 
+	if(chairID == userID || userName == "coord"){
 	// ask for confirmation first
 	int ret = QMessageBox::question(this, qApp->tr("Confirm delete task"),
 			qApp->tr("Are you sure you want to delete this task?.\n"),
@@ -112,9 +188,24 @@ void CommitteeInformation::deleteTask(){
 		QModelIndex index = selected->currentIndex();
 
 		Tmodel->removeRows(index.row(), 1, QModelIndex());
+		}
 	}
+	else {
+		QMessageBox::warning(this, qApp->tr("AAAAAAAAAAAA!"),
+			qApp->tr("YOU CAN'T DO THAT!!!!.\n"),
+			QMessageBox::Ok);
+	}
+
 }
 void CommitteeInformation::markTaskCompleted(){
+	QString text = "SELECT chair_id FROM committees WHERE name = " + committeeName;
+	QSqlQuery query(text);
+	QString chairID;
+	while(query.next()){
+		chairID = query.value(0).toString();
+	}
+
+	if(chairID == userID || userName == "coord"){
 	QItemSelectionModel *selected = taskView->selectionModel();
 	QModelIndex index = selected->currentIndex();
 
@@ -124,14 +215,95 @@ void CommitteeInformation::markTaskCompleted(){
 	QSqlQuery query;
 	query.exec(toChange);
 	Tmodel->select();
+	}
+	else {
+		QMessageBox::warning(this, qApp->tr("AAAAAAAAAAAA!"),
+			qApp->tr("YOU CAN'T DO THAT!!!!.\n"),
+			QMessageBox::Ok);
+	}
 }
 void CommitteeInformation::printCommittee(){
 }
 void CommitteeInformation::printTask(){
 }
 void CommitteeInformation::promoteChair(){
-	// check user is coord or chair of current committee
+	QString text = "SELECT chair_id FROM committees WHERE name = " + committeeName;
+	QSqlQuery query(text);
+	QString chairID;
+	while(query.next()){
+		chairID = query.value(0).toString();
+	}
+
+	if(chairID == userID || userName == "coord"){
+		QItemSelectionModel *selected = constituentView->selectionModel();
+		QModelIndex index = selected->currentIndex();
+
+		QSqlRecord record = CLmodel->record(index.row());
+		QString newChairID = record.value(0).toString();
+		
+		int ret = QMessageBox::question(this, qApp->tr("Confirm promoting to chair"),
+			qApp->tr("Are you sure you want to make this person a chair?.\n"),
+			QMessageBox::Ok | QMessageBox::Cancel);
 	
+		if(ret == QMessageBox::Ok){
+			QString texty = "UPDATE committees SET chair_id = " + newChairID +" WHERE name = " + committeeName;
+			QSqlQuery query1;
+			query1.exec(texty);
+			QString getChairName = "SELECT first_name, last_name FROM users WHERE id = " + newChairID;
+	
+		QSqlQuery query2(getChairName);
+	
+		while ( query2.next() ) {
+			getChairName = query2.value(0).toString();
+			getChairName += " " + query2.value(1).toString();
+		}
+		cNameLabel->setText(getChairName);
+		}
+		
+	} else {
+		QMessageBox::warning(this, qApp->tr("AAAAAAAAAAAA!"),
+			qApp->tr("YOU CAN'T DO THAT!!!!.\n"),
+			QMessageBox::Ok);
+	}
 }
 void CommitteeInformation::promoteSecretary(){
+	QString text = "SELECT chair_id FROM committees WHERE name = " + committeeName;
+	QSqlQuery query(text);
+	QString chairID;
+	while(query.next()){
+		chairID = query.value(0).toString();
+	}
+
+	if(chairID == userID || userName == "coord"){
+		QItemSelectionModel *selected = constituentView->selectionModel();
+		QModelIndex index = selected->currentIndex();
+
+		QSqlRecord record = CLmodel->record(index.row());
+		QString newSecretaryID = record.value(0).toString();
+		
+		int ret = QMessageBox::question(this, qApp->tr("Confirm promoting to secretary"),
+			qApp->tr("Are you sure you want to make this person a secretary?.\n"),
+			QMessageBox::Ok | QMessageBox::Cancel);
+	
+		if(ret == QMessageBox::Ok){
+			QString texty = "UPDATE committees SET secretary_id = " + newSecretaryID +" WHERE name = " + committeeName;
+			QSqlQuery query1;
+			query1.exec(texty);
+			QString getSecretaryName = "SELECT first_name, last_name FROM users WHERE id = " + newSecretaryID;
+	
+		QSqlQuery query2(getSecretaryName);
+	
+		while ( query2.next() ) {
+			getSecretaryName = query2.value(0).toString();
+			getSecretaryName += " " + query2.value(1).toString();
+		}
+		sNameLabel->setText(getSecretaryName);
+		}
+		
+	} else {
+		QMessageBox::warning(this, qApp->tr("AAAAAAAAAAAA!"),
+			qApp->tr("YOU CAN'T DO THAT!!!!.\n"),
+			QMessageBox::Ok);
+	}
+	
 }
